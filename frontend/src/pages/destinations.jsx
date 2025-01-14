a001ef1bed3a26342dcd17ce75c1a79ef0649548
THILAK0508 <thilakpatel34@gmail.com>
2025-01-14 17:37:52 +0530

    destinations-2

--- frontend/src/pages/destinations.jsx
+++ frontend/src/pages/destinations.jsx
@@ -3,7 +3,7 @@ import "../css/destinations.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const API_URL = `http://${API_BASE_URL}/api/cards`;  // Change this if your server is hosted somewhere else
+const API_URL = `${API_BASE_URL}/api/cards`;  // Corrected API URL
 
 function Destination() {
   const [cardsData, setCardsData] = useState([]);
@@ -27,39 +27,26 @@ function Destination() {
   return (
     <div>
       <Header />
-
-      {/* World Section (Popular Destinations) */}
-      <div id="world">
-        <h4>World</h4>
-        <div className="card-container">
-          {cardsData.map((card) => (
-            <div className="card" key={card._id}>
-              <img src={card.image} alt={card.title} />
-              <div style={{ background: "white" }}>
-                <h5>{card.title}</h5>
-                <p>{card.description}</p>
-              </div>
+      <div className="destinations-container">
+        <h2>Popular Destinations</h2>
+        <div className="cards-container">
+          {cardsData.map((card, index) => (
+            <div key={index} className="card">
+              <h3>{card.title}</h3>
+              <p>{card.description}</p>
+            </div>
+          ))}
+        </div>
+        <h2>Near You</h2>
+        <div className="cards-container">
+          {nearYouData.map((card, index) => (
+            <div key={index} className="card">
+              <h3>{card.title}</h3>
+              <p>{card.description}</p>
             </div>
           ))}
         </div>
       </div>
-
-      {/* Near You Section */}
-      <div id="world">
-        <h4>Near You</h4>
-        <div className="card-container">
-          {nearYouData.map((card) => (
-            <div className="card" key={card._id}>
-              <img src={card.image} alt={card.title} />
-              <div style={{ background: "white" }}>
-                <h5>{card.title}</h5>
-                <p>{card.description}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
       <Footer />
     </div>
   );
